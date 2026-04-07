6c83fef0a381cc9a04000fd0d01555a7ea85d71f
smirnov-ilya <97843633+smirnov-ilya@users.noreply.github.com>
2026-04-07 19:17:42 +0200

    Update update_cv.py

--- update_cv.py
+++ update_cv.py
@@ -86,8 +86,7 @@ def generate_html():
         # Construct the HTML list item
         item = f'<li class="paper-item" data-category="{category}" data-year="{year}">\n'
         item += f'  <div class="title-row"><strong>{title_html}</strong></div>\n'
-        item += f'  <b>{author}</b>, '
-        item += f'  <div class="source-row">{source}.</div>\n'
+        item += f'  <b>{author}</b>. {source}.\n'
                     
         item += '</li>\n'
         html_output += item
